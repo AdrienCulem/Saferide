66f888e388cf6697e0cbb7b41809a473bb77efc8
Adrien Culem <culemadrien@outlook.com>
2017-04-29 11:22:38 +0200

    Adding keys file

---
+++ Saferide.Web/Keys.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Saferide.Web
+{
+    public static class Keys
+    {
+        internal static string SendGridKey = "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";
+    }
+}
